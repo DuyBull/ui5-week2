224fc1b97082304716bfafd88624eaa1f82382dd
Thai Xuan Duy <thaixuanduy95@gmail.com>
2021-02-16 23:55:16 +0700

    commit 20210216_002

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,12 +2,17 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"../model/formatter",
 	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
+	"sap/ui/model/FilterOperator",
+	"sap/ui/core/UIComponent"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
 	function (Controller,
+	formatter,
+	Filter,
+	FilterOperator,
+	UIComponent,
 	formatter,
 	Filter,
 	FilterOperator) {
@@ -30,14 +35,35 @@ sap.ui.define([
 					oMessage.show(oResourceBundle.getText("search") + sValue);
 				}.bind(this) );
 
-				var sCity = this.byId('city').getValue(),
-				sGenre    = this.byId('genre').getSelectedItem().getKey(),
-				oCalendar = this.byId('calendar'),
-				oRowBinding = oCalendar.getBinding("rows"),
-				oFilterGenre,
-				oFilterCity;
+				var sCity       = this.byId('city').getValue();
+				var sGenre      = this.byId('genre').getSelectedItem().getKey();
+				var oCalendar   = this.byId('calendar');
+				var oRowBinding = oCalendar.getBinding("rows");
+				var oFilterGenre;
+				var oFilterCity;
 
-				
+				// Create filter 
+				oFilterCity = sCity ? new Filter("info", FilterOperator.Contains, sCity) : null;
+				oFilterGenre = sGenre ? new Filter("genre", FilterOperator.EQ, sGenre) : null;
+
+				// Apply genre filter to rows
+				oRowBinding.filter(oFilterGenre);
+
+				// Apply city filter to row appointments
+				var aRows = oCalendar.getAggregation("rows");
+				aRows.forEach(function (oItem) {
+					var oAppointmentsBinding = oItem.getBinding("appointments");
+					oAppointmentsBinding.filter(oFilterCity);
+				})
+			},
+			onAppointmentSelect: function (oAppointment) {
+				var oContext = oAppointment.getBindingContext("movies");
+				var sPath    = oContext.getPath();
+				var aParameter = sPath.split("/");
+				UIComponent.getRouterFor(this).navTo("Detail", {
+					movieId: aParameter[2],
+					appointmentId: aParameter[4]
+				});
 			}
 		});
 	});
